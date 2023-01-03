94549224553666c073bef7d5db9d6114d63d829e
AY-BAMZ <aybamz181@gmail.com>
2023-01-03 23:57:55 +0100

    app creation and font implementation

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const getFonts = () =>
+  Font.loadAsync({
+    "outfit-thin": require("./assets/font/Outfit-Thin.ttf"),
+    "outfit-extralight": require("./assets/font/Outfit-ExtraLight.ttf"),
+    "outfit-light": require("./assets/font/Outfit-Light.ttf"),
+    "outfit-regular": require("./assets/font/Outfit-Regular.ttf"),
+    "outfit-medium": require("./assets/font/Outfit-Medium.ttf"),
+    "outfit-semibold": require("./assets/font/Outfit-SemiBold.ttf"),
+    "outfit-bold": require("./assets/font/Outfit-Bold.ttf"),
+    "outfit-extrabold": require("./assets/font/Outfit-ExtraBold.ttf"),
+    "outfit-black": require("./assets/font/Outfit-Black.ttf"),
+  });
+
+
 export default function App() {
   return (
     <View style={styles.container}>
