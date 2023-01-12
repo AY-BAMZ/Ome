f6cf9365613cc739e86eeb185401e90e6ce5a323
AY-BAMZ <aybamz181@gmail.com>
2023-01-12 23:20:38 +0100

    notification page

--- screens/DrawerTabs/Header.js
+++ screens/DrawerTabs/Header.js
@@ -1,18 +1,30 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { globalStyles } from "../../styles/global";
+import Notification from "./Notification";
+import { Modal } from "react-native-paper";
 
 export default function Header({title}) {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.header}>
       <View style={globalStyles.headerIconBox}>
         <Ionicons name="menu" size={28} color="#2B2A30" />
       </View>
       <Text style={globalStyles.textFive}>{title}</Text>
-      <View style={globalStyles.headerIconBox}>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}>
+          <Notification setModalVisible={setModalVisible}/>
+        </Modal>
+      <TouchableOpacity onPress={() => setModalVisible(true)} style={globalStyles.headerIconBox}>
         <Ionicons name="notifications-outline" size={28} color="#2B2A30" />
-      </View>
+      </TouchableOpacity>
     </View>
   );
 }
